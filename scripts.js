a8e06af781d0ba2e59dd55a4cac7629e49728f99
Lucas Viana <70477354+lucasvlviana@users.noreply.github.com>
2024-12-03 17:39:48 -0300

    aprendendo variaveis

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Declara uma variável sem valor.
+
+var user
+
+console.log(user);
+
+// Declara uma variável com valor.
+
+var email = "lucasvitorlv@gmail.com";
+console.log(email);
+
+// Substitui o valor da variavel
+email = "lucasvitorlv@outlook.com";
+console.log(email);
+
+var product = "Teclado Mecânico";
+var Product = "Mouse Gamer";
+console.log(product);
+console.log(Product);
