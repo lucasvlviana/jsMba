5658c97c097b38659b3fdc625b3a1de5bcc42da2
Lucas Viana <70477354+lucasvlviana@users.noreply.github.com>
2024-12-03 17:53:09 -0300

    aprendendo constante

--- scripts.js
+++ scripts.js
@@ -17,3 +17,17 @@ var product = "Teclado Mecânico";
 var Product = "Mouse Gamer";
 console.log(product);
 console.log(Product);
+
+// Declara uma variável sem valor.
+
+let User
+console.log(User);
+
+// Declara uma variável com valor.
+
+let Email = "lucasvlviana@icloud.com";
+console.log(Email);
+
+// Uma constante cria uma variável cujo valor é fixo (não pode ser alterado).
+const number = 42;
+console.log(number);
